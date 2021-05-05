ea5d1d72a239a227efad97b0310625e8fd83c3de
longphan110920 <78297881+longphan110920@users.noreply.github.com>
2021-05-05 18:07:54 +0700

    Delete ncc.java

--- ncc.java
+++
@@ -1,9 +0,0 @@
-package ktrabai1;
-import java.util.Scanner;
-public class ncc {
-	public static void main(String[] args) {
-		nhacungcap ncc = new nhacungcap();
-		ncc.nhapThongTin();
-		ncc.xuatThongTin();
-	}
-}
