c1ae767f9518a1eafdc42739cdd39abeac2acea3
longphan110920 <78297881+longphan110920@users.noreply.github.com>
2021-05-05 18:08:06 +0700

    Add files via upload

---
+++ ncc.java
@@ -0,0 +1,9 @@
+package ktrabai1;
+import java.util.Scanner;
+public class ncc {
+	public static void main(String[] args) {
+		nhacungcap ncc1 = new nhacungcap();
+		ncc1.nhapThongTin();
+		ncc1.xuatThongTin();
+	}
+}
